3579ec48ed7a94ff2ddfddfd1505ced7f9f3789f
Ghost <1129.gh0st@gmail.com>
2023-01-12 15:47:08 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LearnmoreComponent } from './learnmore/learnmore.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    LearnmoreComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
